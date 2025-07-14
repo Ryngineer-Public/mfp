0705d77425f19f56fc05cca4733930b5c02202eb
rxr281 <ryan.rodrigues@sherwin.com>
2025-07-14 13:22:18 -0400

    Fixed Header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,27 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 export default () => {
+  // the `createGenerateClassName` function is used to generate unique class names for Material-UI components.
+  // in this case the css keys would be names as mkt1 , mkt2 etc
+  const generateClassName = createGenerateClassName({
+    productionPrefix: "con", // Prefix for class names in production
+    disableGlobal: true, // Disable global styles to avoid conflicts
+  });
   return (
-    <BrowserRouter>
-      <div>
-        {/* This is a warpper component. This is required as App.js can return only react element */}
-        {/* 1. load the marketing app */}
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider injectFirst generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          {/* This is a warpper component. This is required as App.js can return only react element */}
+          {/* 1. load the marketing app */}
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -4,15 +4,25 @@ import {
   Route,
   BrowserRouter,
 } from "react-router-dom/cjs/react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
+// the `createGenerateClassName` function is used to generate unique class names for Material-UI components.
+// in this case the css keys would be names as mkt1 , mkt2 etc
+const generateClassName = createGenerateClassName({
+  productionPrefix: "mkt", // Prefix for class names in production
+  disableGlobal: true, // Disable global styles to avoid conflicts
+});
+
 export default () => {
   return (
     <BrowserRouter>
-      <StylesProvider injectFirst>
+      <StylesProvider injectFirst generateClassName={generateClassName}>
         <Switch>
           <Route path="/" exact component={Landing} />
           <Route path="/pricing" exact component={Pricing} />
