eb5f20c4553e9b5459ee3be51c98d7e307e99a90
rxr281 <ryan.rodrigues@sherwin.com>
2025-07-14 12:23:41 -0400

    Added Header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom";
 export default () => {
   return (
-    <div>
-      {/* This is a warpper component. This is required as App.js can return only react element */}
-      {/* 1. load the marketing app */}
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        {/* This is a warpper component. This is required as App.js can return only react element */}
+        {/* 1. load the marketing app */}
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
